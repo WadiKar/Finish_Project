4ffc154d948d570f5c22bad754826070fa5d9592
WadiKar <karolina.wadolowska96@gmail.com>
2022-11-28 15:24:51 +0100

    first commit

---
+++ Help_Brain/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HelpBrainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Help_Brain'
---
+++ Media/models.py
@@ -0,0 +1,49 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+# Create your models here.
+class Autor(models.Model):
+    full_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.full_name
+
+
+
+class Book(models.Model):
+    GRADE = (
+        (0, ''),
+        (1, '*    '),
+        (2, '**   '),
+        (3, '***  '),
+        (4, '**** '),
+        (5, '*****')
+    )
+    title = models.CharField(max_length=150)
+    no_edition=models.IntegerField()
+    year = models.IntegerField()
+    rating = models.IntegerField(choices=GRADE)
+    autors = models.ManyToManyField(Autor, related_name="books", through="Audiobook" )
+    kategoris = models.ManyToManyField('Category')
+
+
+
+class Audiobook(models.Model):
+    time = models.IntegerField()
+    GRADE = (
+        (0, ''),
+        (1, '*    '),
+        (2, '**   '),
+        (3, '***  '),
+        (4, '**** '),
+        (5, '*****')
+    )
+
+    opinion = models.IntegerField(choices=GRADE)
+    autors = models.ManyToManyField(Autor,  related_name="audiobooks", through="Book")
+    kateg = models.ManyToManyField('Category')
+    book = models.ForeignKey(Book, blank = True, null = True, on_delete=models.CASCADE,)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
